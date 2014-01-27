c837f4bb18886f59e062e5e28cbe59037b14b5b8
Adrian Negreanu <groleo@gmail.com>
2014-01-27 16:46:14 +0200

    colorize.py

--- bin/colorize.py
+++ bin/colorize.py
@@ -15,18 +15,22 @@ BLUE  ="\x1b[1;34m"
 NO_COLOUR="\x1b[0m"
 
 
-def printColored(line):
-	m = re.search('([^:]*):([0-9]+)([:0-9]*)(: [a-zA-Z]*error:)(.*)', line)
-	if m != None:
-		sys.stderr.write(RED+m.group(1)+CYAN+':'+m.group(2)+m.group(3)+YELLOW+m.group(4)+RED+m.group(5)+NO_COLOUR+'\n')
-		return
+def printColored(lines):
+	if not lines:
+		return False
+	for line in lines:
+		m = re.search('([^:]*):([0-9]+)([:0-9]*)(: [a-zA-Z]*error:)(.*)', line)
+		if m != None:
+			sys.stderr.write(RED+m.group(1)+CYAN+':'+m.group(2)+m.group(3)+YELLOW+m.group(4)+RED+m.group(5)+NO_COLOUR+'\n')
+			continue
 
-	m = re.search('([^:]*):([0-9]+)([:0-9]*)(: [a-zA-Z]*warning:)(.*)', line)
-	if m != None:
-		sys.stderr.write(RED+m.group(1)+CYAN+':'+m.group(2)+m.group(3)+GREEN+m.group(4)+NO_COLOUR+m.group(5)+NO_COLOUR+'\n')
-		return
+		m = re.search('([^:]*):([0-9]+)([:0-9]*)(: [a-zA-Z]*warning:)(.*)', line)
+		if m != None:
+			sys.stderr.write(RED+m.group(1)+CYAN+':'+m.group(2)+m.group(3)+GREEN+m.group(4)+NO_COLOUR+m.group(5)+NO_COLOUR+'\n')
+			continue
 
-	sys.stdout.write(line)
+		sys.stdout.write(line)
+	return True
 
 def setnonblock(fd):
 	fl = fcntl.fcntl(fd, fcntl.F_GETFL)
@@ -34,16 +38,15 @@ def setnonblock(fd):
 
 
 if len(sys.argv)==1:
-	r = [sys.stdin.fileno()]
 	while True:
-		ready = select.select(r,[],[],1)
+		r = [sys.stdin.fileno()]
+		ready = select.select(r,[],[])
 		for fd in ready[0]:
 			if fd == sys.stdin.fileno():
 				lines = sys.stdin.readlines()
-				for line in lines:
-					printColored(line)
-					sys.stdout.flush()
-					sys.stderr.flush()
+				if not printColored(lines): sys.exit(0)
+				sys.stdout.flush()
+				sys.stderr.flush()
 elif sys.stdin.isatty():
 	p = subprocess.Popen(sys.argv[1:],
 			bufsize=1,
@@ -57,8 +60,8 @@ elif sys.stdin.isatty():
 
 	for fd in ready[0]:
 		if fd == p.stdout.fileno():
-			for line in p.stdout:
-				printColored(line)
+			lines = p.stdout.readlines()
+			printColored(lines)
 		if fd == p.stderr.fileno():
-			for line in p.stderr:
-				printColored(line)
+			lines = p.stderr.readlines()
+			printColored(lines)
